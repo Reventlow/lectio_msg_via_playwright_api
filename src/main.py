d7831865db5bd9a8b6b4cd27297a72b44a1fdf90
Gorm Reventlow <gorm@reventlow.com>
2025-01-16 09:16:35 +0100

    implementing dashboard

--- src/main.py
+++ src/main.py
@@ -237,7 +237,7 @@ def get_dashboard():
     </div>
 
     <script>
-        const ws = new WebSocket("ws://lectio_api:8000/ws/dashboard");
+        const ws = new WebSocket("ws://10.18.225.150:8010/ws/dashboard");
 
         ws.onmessage = (event) => {
             const data = JSON.parse(event.data);
